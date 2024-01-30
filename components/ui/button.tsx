e98d29dd450d864ee061ee969fef7d4808ecb820
sam <samuel6249ng@gmail.com>
2024-01-31 01:54:24 +1100

    fetched category pages and side modal and working on cart

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -37,7 +37,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps> (({
         ref={ref}
         {...props}
         >
-            {children}
+        {children}
         </button>
     )
 });
