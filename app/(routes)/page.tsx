a8b917ae5cc718042cd1a9d5d3e85c184375da35
sam <samuel6249ng@gmail.com>
2024-02-03 13:10:12 +1100

    Remove all module files

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -11,7 +11,7 @@ const HomePage = async () => {
   const products = await getProducts({ isFeatured: true })
 
   // copy the API from billboard CMS
-  const billboard = await getBillboard("879fcd0b-ddcd-4461-8b0b-47e3edeaebe8");
+  const billboard = await getBillboard("9ebdb4ac-9218-4ea9-8eeb-98595c6b78bb");
   
   return (
     <Container>
