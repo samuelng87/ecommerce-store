f5affd52729ec124a347cfb150687dbc3e6e3a57
sam <samuel6249ng@gmail.com>
2024-02-05 21:45:52 +1100

    responsive images

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-import  Container  from "@/components/ui/container"
+import Container  from "@/components/ui/container"
 import Link from "next/link"
 import MainNav from "@/components/main-nav"
 import getCategories from "@/actions/get-categories";
